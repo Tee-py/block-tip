7f2e9ab9ac0380b4112c7a9910628fdcb8abb12d
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2024-10-13 06:26:52 +0100

    added abis

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'hardhat-abi-exporter';
+import 'dotenv/config';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: {
+    version: '0.8.4',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  abiExporter: {
+    runOnCompile: true,
+    only: ['SocialDonation'],
+    path: './abis',
+    format: 'json',
+    clear: true,
+  },
 };
 
 export default config;
